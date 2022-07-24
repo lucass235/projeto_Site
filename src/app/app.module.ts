26072f04d67fb7612957aa926a94f4bd6c87f06a
Lucas dos Santos Amorim Rêgo <lucass235@gmail.com>
2022-07-24 00:12:32 -0300

    Iniciando estrutura do porojeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageComponent } from './main/home/page/page.component';
+import { HeaderComponent } from './core/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
